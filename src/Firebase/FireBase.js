75fe345c57a9dd97939c6c30e32ee97bbc7c5a6a
Neeraj Kumar Mahawar <neerajkmahawar2017@gmail.com>
2021-09-14 11:28:48 +0530

    Change DB to neer....2.0@gmail.com

--- src/Firebase/FireBase.js
+++ src/Firebase/FireBase.js
@@ -1,13 +1,12 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBkVdtyQkr6nIj9cCi9vKvGxZL8qzQQ6n4",
-  authDomain: "postdata-b31f3.firebaseapp.com",
-  databaseURL: "https://postdata-b31f3-default-rtdb.firebaseio.com",
-  projectId: "postdata-b31f3",
-  storageBucket: "postdata-b31f3.appspot.com",
-  messagingSenderId: "648323993769",
-  appId: "1:648323993769:web:b534b8756fa532ba8459b8",
+  apiKey: "AIzaSyACOP0VOWYCUX9ihtwdjiRNxdSq0F9gwU8",
+  authDomain: "nk-messenger-2567a.firebaseapp.com",
+  projectId: "nk-messenger-2567a",
+  storageBucket: "nk-messenger-2567a.appspot.com",
+  messagingSenderId: "505850474441",
+  appId: "1:505850474441:web:0f9517622f5d5046910218",
 });
 
 const db = firebaseApp.firestore();
